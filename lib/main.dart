084ab69777092cf432f9043e6a855a2797b59d60
GabriFe <gabri10.fernandes23@gmail.com>
2024-04-15 20:49:27 -0300

    adicionando teste

--- lib/main.dart
+++ lib/main.dart
@@ -89,6 +89,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
+          // teste
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
